2ab4a9a607392ad770650e312e72566f5c29d976
Minji <120402129+meanzi3@users.noreply.github.com>
2024-03-13 21:42:06 +0900

    Time: 0 ms (100.00%), Space: 42 MB (8.87%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,29 +16,16 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        TreeNode cur = root;
-        
-        while (cur != null) { 
-            if (cur.left != null) { // 왼쪽 노드가 있으면
-                TreeNode pre = cur.left;
-                while ((pre.right != null) && (pre.right != cur)){
-                    pre = pre.right;
-                }
-                if(pre.right == null){
-                    pre.right = cur;
-                    cur = cur.left;
-                } else {
-                    pre.right = null;
-                    answer.add(cur.val);
-                    cur = cur.right;
-                }
-            } else { // 왼쪽 노드가 없으면
-                // 현재 값 넣고
-                answer.add(cur.val);
-                // 오른쪽으로
-                cur = cur.right;
-            }
-        }
+        inorder(root, answer);
         return answer;
     }
+
+    private void inorder(TreeNode node, List<Integer> answer){
+        if (node != null){
+            if(node.left != null) {inorder(node.left, answer);}
+            answer.add(node.val);
+            if(node.right != null) {inorder(node.right, answer);}
+        } 
+    }
+
 }
